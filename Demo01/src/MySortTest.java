c6708bab3c475c7ca95d1a678ac1283dd07b7d4c
kadphol <kadphol.len@outlook.co.th>
2021-06-17 18:06:29 +0700

    Add Two items case 02

--- Demo01/src/MySortTest.java
+++ Demo01/src/MySortTest.java
@@ -1,13 +1,16 @@
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Collections;
 import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class MySortTest {
     @Test
+    @DisplayName("Test Sort with Empty List")
     public void EmptySort() {
         MySort mysort = new MySort(new ArrayList<>());
         List<Integer> result = mysort.sort();
@@ -15,16 +18,27 @@ class MySortTest {
     }
 
     @Test
+    @DisplayName("Test Sort with Single items in List")
     public void OneElementList() {
-        MySort mysort = new MySort(Arrays.asList(1));
+        MySort mysort = new MySort(Collections.singletonList(1));
         List<Integer> result = mysort.sort();
-        assertEquals(Arrays.asList(1), result);
+        assertEquals(Collections.singletonList(1), result);
     }
 
     @Test
+    @DisplayName("Test Sort with [1,2]" + "expected result [1,2]")
     public void TwoElementListCase01() {
         MySort mysort = new MySort(Arrays.asList(1,2));
         List<Integer> result = mysort.sort();
         assertEquals(Arrays.asList(1,2),result);
     }
+
+    @Test
+    @DisplayName("Test Sort with [2,1]" + "expected result [1,2]")
+    public void TwoElementListCase02() {
+        MySort mysort = new MySort(Arrays.asList(2,1));
+        List<Integer> result = mysort.sort();
+        assertEquals(Arrays.asList(1,2),result);
+    }
+
 }
