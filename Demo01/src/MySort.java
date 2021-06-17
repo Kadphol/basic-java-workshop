b02e9f437524af24fd5f6a1adb053ba15f013d28
kadphol <kadphol.len@outlook.co.th>
2021-06-17 17:05:05 +0700

    Add empty case

---
+++ Demo01/src/MySort.java
@@ -0,0 +1,17 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class MySort {
+    List<Integer> input;
+    public MySort(ArrayList<Integer> input) {
+        this.input = input;
+    }
+
+    public List<Integer> sort() {
+        if(input.size() <= 1) {
+            return input;
+        }
+
+        return input;
+    }
+}
---
+++ Demo01/src/MySortTest.java
@@ -0,0 +1,15 @@
+import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class MySortTest {
+    @Test
+    public void EmptySort() {
+        MySort mysort = new MySort(new ArrayList<>());
+        List<Integer> result = mysort.sort();
+        assertEquals(new ArrayList<>(), result);
+    }
+}
