35bdb4fa8a3b78b2a0131aa46decac3b4384f9d9
kadphol <kadphol.len@outlook.co.th>
2021-06-21 10:31:48 +0700

    init hello-res-maven

---
+++ hello-rest-maven/src/main/java/com/example/hellorestmaven/HelloRestMavenApplication.java
@@ -0,0 +1,13 @@
+package com.example.hellorestmaven;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HelloRestMavenApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HelloRestMavenApplication.class, args);
+	}
+
+}
---
+++ hello-rest-maven/src/test/java/com/example/hellorestmaven/HelloRestMavenApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.hellorestmaven;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HelloRestMavenApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
