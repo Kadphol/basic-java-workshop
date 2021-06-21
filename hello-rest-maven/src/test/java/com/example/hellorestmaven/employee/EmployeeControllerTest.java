cab7d9a758d783f9bd1268dcee60fe873800e823
kadphol <kadphol.len@outlook.co.th>
2021-06-21 14:03:50 +0700

    initial controller test

---
+++ hello-rest-maven/src/test/java/com/example/hellorestmaven/employee/EmployeeControllerTest.java
@@ -0,0 +1,15 @@
+package com.example.hellorestmaven.employee;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.springframework.boot.test.context.SpringBootTest.*;
+
+@SpringBootTest(webEnvironment = WebEnvironment.RANDOM_PORT)
+class EmployeeControllerTest {
+    @Test
+    public void callApiWithPathVariable() {
+
+    }
+}
