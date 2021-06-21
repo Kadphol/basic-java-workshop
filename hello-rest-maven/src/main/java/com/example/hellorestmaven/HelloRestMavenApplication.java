5d88eccb958242810aecc9963898773c902195f9
kadphol <kadphol.len@outlook.co.th>
2021-06-21 15:01:39 +0700

    Initial bean context

--- hello-rest-maven/src/main/java/com/example/hellorestmaven/HelloRestMavenApplication.java
+++ hello-rest-maven/src/main/java/com/example/hellorestmaven/HelloRestMavenApplication.java
@@ -1,13 +1,29 @@
 package com.example.hellorestmaven;
 
+import com.example.hellorestmaven.employee.Demo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
 
 @SpringBootApplication
 public class HelloRestMavenApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(HelloRestMavenApplication.class, args);
+
+		ConfigurableApplicationContext context
+				= SpringApplication.run(HelloRestMavenApplication.class, args);
+		String[] objects = context.getBeanDefinitionNames();
+		for (String object : objects) {
+			System.out.println(object);
+		}
+		int count = context.getBeanDefinitionCount();
+		System.out.println("Total=" + count);
+
+		Demo demo1 = context.getBean(Demo.class);
+		demo1.setName("From demo1");
+		Demo demo2 = context.getBean(Demo.class);
+		System.out.println("Demo1 => " +demo1.getName());
+		System.out.println("Demo2 => " +demo2.getName());
 	}
 
 }
