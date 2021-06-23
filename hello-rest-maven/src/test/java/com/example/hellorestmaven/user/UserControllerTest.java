d600a93a3994625831d8355307c845e009f186fe
Kadphol <kadphol.len@outlook.co.th>
2021-06-23 15:30:39 +0700

    Test UserController getUsers

--- hello-rest-maven/src/test/java/com/example/hellorestmaven/user/UserControllerTest.java
+++ hello-rest-maven/src/test/java/com/example/hellorestmaven/user/UserControllerTest.java
@@ -1,7 +1,23 @@
 package com.example.hellorestmaven.user;
 
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+
 import static org.junit.jupiter.api.Assertions.*;
 
-class UserControllerTest {
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+public class UserControllerTest {
+    @Autowired
+    private TestRestTemplate restTemplate;
 
+    @Test
+    public void getUsersTest() {
+        UserModel[] result = restTemplate.getForObject("/users",UserModel[].class);
+        assertEquals(5, result.length);
+        for (UserModel user : result) {
+            assertEquals(user.getId() % 2, 0);
+        }
+    }
 }
