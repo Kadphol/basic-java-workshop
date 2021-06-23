95a5b03e3d6aec53a14640754d12b5df83e77a39
Kadphol <kadphol.len@outlook.co.th>
2021-06-23 10:03:35 +0700

    Add EmployeeRepositoryTest

---
+++ hello-rest-maven/src/test/java/com/example/hellorestmaven/employee/EmployeeRepositoryTest.java
@@ -0,0 +1,22 @@
+package com.example.hellorestmaven.employee;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@DataJpaTest
+public class EmployeeRepositoryTest {
+
+    @Autowired
+    private EmployeeRepository repository;
+
+    @Test
+    public void foundWithID1() {
+        Employee employee1 = repository.getById(1);
+        assertEquals(1, employee1.getId());
+        assertEquals("Someone", employee1.getFirstName());
+        assertEquals("Something", employee1.getLastName());
+    }
+}
