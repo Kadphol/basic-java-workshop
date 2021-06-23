c1451bdf3509e211e4530fe3c88347729b8f1313
Kadphol <kadphol.len@outlook.co.th>
2021-06-23 09:44:51 +0700

    Add employee data

---
+++ hello-rest-maven/src/main/java/com/example/hellorestmaven/employee/Employee.java
@@ -0,0 +1,47 @@
+package com.example.hellorestmaven.employee;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class Employee {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private int id;
+
+    private String firstName;
+    private String LastName;
+
+    public Employee() {}
+
+    public Employee(String firstName, String lastName) {
+        this.firstName = firstName;
+        this.LastName = lastName;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return LastName;
+    }
+
+    public void setLastName(String lastName) {
+        LastName = lastName;
+    }
+}
